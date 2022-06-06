8674aaaaebaf76d37e3d7d465c803616b32fec1b
Deivid Fonseca <deivid.fonseca@hdn.digital>
2022-06-06 17:14:50 -0300

    commit initial.

---
+++ lib/app/micro_app_login_resolver.dart
@@ -0,0 +1,13 @@
+import 'package:micro_app_login/app/page/login_page.dart';
+import 'package:micro_core/app/micro_core_utils.dart';
+import 'package:micro_core/app/microapp.dart';
+
+class MicroAppLoginResolver implements MicroApp {
+  @override
+  String get microAppNamme => throw UnimplementedError();
+
+  @override
+  Map<String, WidgetBuilderArgs> get routes => {
+        "/login": ((context, args) => const LoginPage()),
+      };
+}
